1674d440c1d9ef5dfb16f1e57b128ee7d792a8f3
morita <yasushi-morita@vcube.co.jp>
2019-01-30 10:56:48 +0900

    【AgoraIO-CallCenter】design

--- customer/index.php
+++ customer/index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
-<head>ICUログイン</head>
-<title>ICUログイン</title>
+<head>customerログイン
+    <!-- 20190130 レスポンシブWebデザインを使うために必要なmetaタグ -->
+    <meta name = "viewport" content = "width=device-width, initial-scale=1">
+</head>
+<title>customerログイン</title>
 
 <body>
 <form action="login.php" method="post">
@@ -10,4 +13,13 @@
     <button id="btnLogin">ログイン</button>
 </form>
 </body>
+
+<!-- 20190130 -->
+<!-- BootstrapのCSS読み込み -->
+<link href = "../bootstrap-4.2.1-dist/css/bootstrap.min.css" rel = "stylesheet">
+<!-- jQuery読み込み -->
+<script src = "https://ajax.googleapis.com/ajax/libs/jquery.min.js"></script>
+<!-- BootstrapのJS読み込み -->
+<script src = "../bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
+
 </html>

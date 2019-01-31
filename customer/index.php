16bfafac5fc55d5e1c84eb704890b14999c33af9
morita <yasushi-morita@vcube.co.jp>
2019-01-31 11:32:49 +0900

    【AgoraIO-CallCenter】design
    customer/index.php

--- customer/index.php
+++ customer/index.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
 <html>
-<head>customerログイン
+<head>
     <!-- 20190130 レスポンシブWebデザインを使うために必要なmetaタグ -->
-    <meta name = "viewport" content = "width=device-width, initial-scale=1">
+    <meta name="viewport" content = "width=device-width, initial-scale=1">
+    <!-- 20190130 -->
+    <!-- BootstrapのCSS読み込み -->
+    <link href="../bootstrap-4.2.1-dist/css/bootstrap.min.css" rel = "stylesheet">
+    <!-- jQuery読み込み -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <!-- BootstrapのJS読み込み -->
+    <script src="../bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
 </head>
-<title>customerログイン</title>
 
+<title>customerログイン</title>
 <body>
+<!-- <div class="table-responsive"> -->
+<table class="table table-bordered">
 <form action="login.php" method="post">
-    <input name="id" type="text" value="icu"><br />
-    <input name="pass" type="password" value="password"><br />
-    <button id="btnLogin">ログイン</button>
+    <tr>
+        <th>ID</th>
+        <td><input name="id" type="text" value="customer"></td>
+    </tr>
+    <tr>
+        <th>Password</th>
+        <td><input name="pass" type="password" value="password"></td>
+    </tr>
+    <!-- <tr>
+        <th><button type="button" class="btn btn-primary">ログイン</button></th>
+    </tr> -->
 </form>
+</table>
+<!-- </div> -->
+<button type="button" class="btn btn-primary">ログイン</button>
 </body>
 
-<!-- 20190130 -->
-<!-- BootstrapのCSS読み込み -->
-<link href = "../bootstrap-4.2.1-dist/css/bootstrap.min.css" rel = "stylesheet">
-<!-- jQuery読み込み -->
-<script src = "https://ajax.googleapis.com/ajax/libs/jquery.min.js"></script>
-<!-- BootstrapのJS読み込み -->
-<script src = "../bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
+
 
 </html>
